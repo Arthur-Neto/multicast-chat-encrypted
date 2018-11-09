b4c135a2410a892cb4837b399eb11356d68a909a
Arthur-Neto <arthuraomtt@gmail.com>
2018-11-09 11:44:22 -0200

    Modificado tela

---
+++ cliente/Chat.WinApp/FormPrincipal.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Chat.WinApp
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxConversa = new System.Windows.Forms.TextBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonEnviar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.listBoxOnline = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxConversa
+            // 
+            this.textBoxConversa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxConversa.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxConversa.Location = new System.Drawing.Point(0, 0);
+            this.textBoxConversa.Multiline = true;
+            this.textBoxConversa.Name = "textBoxConversa";
+            this.textBoxConversa.ReadOnly = true;
+            this.textBoxConversa.Size = new System.Drawing.Size(552, 414);
+            this.textBoxConversa.TabIndex = 99;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxInput.Location = new System.Drawing.Point(0, 0);
+            this.textBoxInput.MaxLength = 200;
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(471, 44);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // buttonEnviar
+            // 
+            this.buttonEnviar.Location = new System.Drawing.Point(696, 432);
+            this.buttonEnviar.Name = "buttonEnviar";
+            this.buttonEnviar.Size = new System.Drawing.Size(75, 44);
+            this.buttonEnviar.TabIndex = 2;
+            this.buttonEnviar.Text = "Enviar";
+            this.buttonEnviar.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBoxOnline);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(201, 464);
+            this.panel1.TabIndex = 100;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBoxConversa);
+            this.panel2.Location = new System.Drawing.Point(219, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(552, 414);
+            this.panel2.TabIndex = 101;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBoxInput);
+            this.panel3.Location = new System.Drawing.Point(219, 432);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(471, 44);
+            this.panel3.TabIndex = 102;
+            // 
+            // listBoxOnline
+            // 
+            this.listBoxOnline.FormattingEnabled = true;
+            this.listBoxOnline.Location = new System.Drawing.Point(3, 3);
+            this.listBoxOnline.Name = "listBoxOnline";
+            this.listBoxOnline.Size = new System.Drawing.Size(195, 459);
+            this.listBoxOnline.Sorted = true;
+            this.listBoxOnline.TabIndex = 100;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 485);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.buttonEnviar);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FormPrincipal";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chat";
+            this.TopMost = true;
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxConversa;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonEnviar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ListBox listBoxOnline;
+    }
+}
+
---
+++ cliente/Chat.WinApp/FormPrincipal.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Chat.WinApp
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
